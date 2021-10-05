24de1917bf48834a4fdde7709686eeedb0b5781e
beko6648 <beko6648@gmail.com>
2021-10-05 14:24:51 +0900

    sample.py追記

--- src/python/sample.py
+++ src/python/sample.py
@@ -6,8 +6,13 @@ class date_class:
         self.dt_now = datetime.datetime.now()
         # self.date_data = [self.dt_now.month, self.dt_now.day, self.dt_now.hour, self.dt_now.minute]
         self.access = access
-        
-print(json.dumps(date_class('enter')))
+
+test = {
+	"time_data": "2006-01-02T15:04:05.999Z",
+	"enter_or_leave": "enter"
+}
+
+print(json.dumps(test))
 
 # f = open('./src/python/test.json', 'r')
 # json_dict = json.load(f)
