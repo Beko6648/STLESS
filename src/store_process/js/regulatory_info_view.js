ccd9ae47d52f0c15a6058dbafb101732fc8cd5be
beko6648 <beko6648@gmail.com>
2021-10-21 15:08:04 +0900

    規制状況を受け渡すように

--- src/store_process/js/regulatory_info_view.js
+++ src/store_process/js/regulatory_info_view.js
@@ -15,4 +15,16 @@ $(() => {
             console.log('goto_camera_setting', data);
         })()
     })
+
+    // 店内客数が変化したタイミングで規制情報が送られてくる
+    ipcRenderer.on("update_regulation_info", (event, regulatory_info_obj) => {
+        let number_of_people = regulatory_info_obj.number_of_people;
+        let regulatory_status = regulatory_info_obj.regulatory_status;
+
+        console.log('number_of_people', number_of_people);
+        console.log('regulatory_status', regulatory_status);
+
+        $('#number_of_people').html(number_of_people);
+        $('#regulatory_status').html(regulatory_status);
+    })
 })
