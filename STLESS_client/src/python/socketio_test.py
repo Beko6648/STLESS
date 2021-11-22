90280e4968e4c6c233c9d0d0caa3fb19aae1026b
satoshi.y <satoshi.neru@gmail.com>
2021-11-22 09:35:12 +0900

    socketio_test.py 変更

--- STLESS_client/src/python/socketio_test.py
+++ STLESS_client/src/python/socketio_test.py
@@ -25,15 +25,17 @@ class SocketIOClient:
 
         self.sio.register_namespace(MyCustomNamespace(self.path)) # 名前空間を設定
         self.sio.connect(self.host) # サーバーに接続
-        self.sio.start_background_task(self.my_background_task, 123) # バックグラウンドタスクの登録 (123は引数の書き方の参考のため、処理には使っていない)
-        self.sio.wait() # イベントが待ち
+        # self.sio.start_background_task(self.my_background_task, 123) # バックグラウンドタスクの登録 (123は引数の書き方の参考のため、処理には使っていない)
+        # self.sio.wait() # イベントが待ち
         
     def my_background_task(self, my_argument): # ここにバックグランド処理のコードを書く
-        while True:
-            input_data = input("send data:") # ターミナルから入力された文字を取得
-            self.sio.emit('python', input_data, namespace = self.path) # ターミナルで入力された文字をサーバーに送信
-            self.sio.sleep(1)
+        # while True:
+        #     input_data = input("send data:") # ターミナルから入力された文字を取得
+        self.sio.emit('python', my_argument, namespace = self.path) # ターミナルで入力された文字をサーバーに送信
+        # self.sio.sleep(1)
+            
 
 if __name__ == '__main__':
-    sio_client = SocketIOClient('http://localhost:3000/', '/') # SocketIOClientクラスをインスタンス化
+    sio_client = SocketIOClient('http://10.10.50.184:3000/', '/') # SocketIOClientクラスをインスタンス化
     sio_client.connect()
+    sio_client.my_background_task(21324)
