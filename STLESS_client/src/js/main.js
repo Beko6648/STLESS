65ba6d0f3b95b8088fd73cef31a82f902953d61d
beko6648 <beko6648@gmail.com>
2021-12-06 14:27:50 +0900

    システム終了時刻が来たときに店内客数等を初期化するように

--- STLESS_client/src/js/main.js
+++ STLESS_client/src/js/main.js
@@ -29,8 +29,8 @@ const initial_system_setting = {
 }
 
 // 変数の初期化
-let connection = null;
-let store_window = null;
+let connection = null; // mysqlを利用するための変数
+let store_window = null; // アプリウィンドウ
 let people_in_store_queue = []; // 店内の客を管理するキュー入店時間を値として持っている
 let shopping_time_queue = []; // 入退店データキュー入店時間,退店時間を値として持っている
 let waiting_time_estimation_data = { hour: 0, minute: 10, second: 0 }; // 待ち時間推測用データ 形式{ hour, minute }
@@ -334,8 +334,21 @@ const batch_process = () => {
             });
         })).then(() => {
             console.log('DBへの書き込みが完了しました');
-            // １日分のデータを送信し終わったら、送信済みのデータを
+            // １日分のデータを送信し終わったら、保持中のデータをリセットする
             shopping_time_queue = [];
+            people_in_store_queue = [];
+            next_html = 'allow_entry.html';
+            camera_data[0].enter_count = 0;
+            camera_data[0].leave_count = 0;
+            camera_data[1].enter_count = 0;
+            camera_data[1].leave_count = 0;
+
+            // 店内客数の変化を規制情報確認画面用に通知する
+            store_window.webContents.send('update_regulation_info', {
+                number_of_people: people_in_store_queue.length,
+                regulatory_status: next_html,
+                camera_data: camera_data
+            });
 
             // グラフデータを生成し、アプリストレージに保存する
             generate_graph_data();
